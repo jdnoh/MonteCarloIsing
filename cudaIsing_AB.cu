ac52f1ebd2828eca6e16197aafb395b00c80b048
jdnoh <jaedongnoh@gmail.com>
2021-07-13 22:16:20 +0900

    Monte Carlo simulation of 2D Ising model with CUDA

---
+++ cudaIsing_AB.cu
@@ -0,0 +1,105 @@
+// variable type for spin related variables can be set here
+typedef signed char spin_t;
+// typedef int spin_t;
+
+#include "subIsing_AB.c"
+
+#define MaxThreads (128)
+
+int main(int argc, char *argv[])
+{
+    const int Lsize = atoi(argv[1]); // input of lattice size
+    const int tmax = atoi(argv[2]);  // input of simulation time
+    const int delt = atoi(argv[3]);  // input of measurement interval
+
+    // square lattice with nSites = Lsize*Lsize
+    // whole lattice = sublattices A(even) + B(odd)
+    // |sublattice| = Lx * Ly with Lx = Lsize/2 and Ly = Lsize 
+    const int nSites = Lsize*Lsize; 
+    const int Lx = Lsize/2;
+    const int Ly = Lsize ;
+    const int nSitesHalf = nSites/2;
+
+    // grid configuration
+    // nThread = min[ Lx, MaxThreads], nBlocks = Ly
+    const int nThreads = (Lx < MaxThreads)? Lx : MaxThreads;
+    const int nBlocks = nSitesHalf / nThreads;
+
+    // seeds for CPU and GPU PRNG
+    unsigned int seedCPU, seedGPU;
+    get_PRNG_seeds(&seedCPU, &seedGPU);
+    seedCPU = 1234; seedGPU = 1234;
+    printf("# seedCPU = %u seedGPU = %u\n", seedCPU, seedGPU);
+
+    // CPU PRNG setup
+    // Mersenne-Twrister engine initialized with seedCPU
+    std::mt19937 cpuPRNG(seedCPU);
+    // random number distribution type: uniform in [0,1)
+    std::uniform_real_distribution<float> rand_uniform(0.0, 1.0);
+    // usage: r = rand_uniform(cpuPRNG)
+
+    // GPU PRNG setup
+    curandGenerator_t gen;
+    // default (WOWXOR) engine
+    curandCreateGenerator(&gen, CURAND_RNG_PSEUDO_DEFAULT);
+    // initialization with the seedGPU
+    curandSetPseudoRandomGeneratorSeed(gen, seedGPU);
+
+    // device memory for spins, local magnetization, and energy
+    // variable type: "int" (4 byte) or "signed int" (1 byte)
+    // spin_t = int or signed int 
+    spin_t *devSpinA, *devSpinB, *devMaglocal, *devElocal;
+
+    // memory allocation
+    prepare1_memory(&devSpinA, &devSpinB, &devMaglocal, 
+            &devElocal, nSitesHalf);
+
+    // memory for random numbers and Boltzmann factors
+    float *devRand, *devBoltzmannFactor;
+    prepare2_memory(&devBoltzmannFactor, &devRand, nSitesHalf);
+    
+    float beta = log(1.+sqrt(2.))/2.0;
+    // for(float beta=0.3; beta<0.6; beta += 0.02) {
+        // setup of Boltzmann factor e^{-beta dE} 
+        set_weight(devBoltzmannFactor, beta);
+
+        // random initial spin configuration
+        curandGenerateUniform(gen, devRand, nSitesHalf);
+        init_rand_spins<<<nBlocks, nThreads>>>(devSpinA, devRand);
+        curandGenerateUniform(gen, devRand, nSitesHalf);
+        init_rand_spins<<<nBlocks, nThreads>>>(devSpinB, devRand);
+
+        for(int t=1; t<=tmax; t++) {
+            // random numbers generation 
+            curandGenerateUniform(gen, devRand, nSitesHalf);
+
+            // random sublattice update
+            if(rand_uniform(cpuPRNG)<0.5) 
+                // A sublattice update
+                spinFlip_AB<<<nBlocks, nThreads>>>(0, Lx, Ly, devSpinA,
+                    devSpinB, devBoltzmannFactor, devRand);
+            else
+                // or B sublattice update
+                spinFlip_AB<<<nBlocks, nThreads>>>(1, Lx, Ly, devSpinB,
+                    devSpinA, devBoltzmannFactor, devRand);
+
+            if(t%delt==0) {  // measurement
+                long int magnetization, energy;
+                get_orderParameter(Lx, Ly, nSitesHalf, devSpinA, 
+                        devSpinB, devMaglocal, devElocal,
+                        &magnetization, &energy,
+                        nBlocks, nThreads);
+                // output 
+                printf("%8d %18e %12d %12ld %12ld\n", Lsize, beta, t, 
+                        energy, magnetization);
+            }
+
+            // thread synchronization
+            // not necessary, but just for the peace of mind
+            cudaDeviceSynchronize(); 
+        }
+    //}
+
+    cudaFree(devBoltzmannFactor); cudaFree(devRand); 
+    cudaFree(devSpinA); cudaFree(devSpinB), cudaFree(devElocal);
+}
---
+++ subIsing_AB.c
@@ -0,0 +1,148 @@
+#include <stdio.h>
+#include <math.h>
+#include <curand.h>
+#include <cuda.h>
+#include <random>
+#include <iostream>
+#include <thrust/device_vector.h>
+#include <thrust/host_vector.h>
+#include <thrust/reduce.h>
+
+const int nNeighbor = 4;
+
+// random sublattice spin flips using the Metropolis algorithm
+// A (B) sublattice update when even_odd = 0 (1)
+// neighbors of an A site (x,y) = (x,y+1), (x,y-1), (x-(-1)^y,y), (x,y) 
+// neighbors of a  B site (x,y) = (x,y+1), (x,y-1), (x+(-1)^y,y), (x,y)
+__global__ void spinFlip_AB(int even_odd, const int lx, const int ly, 
+        spin_t *spinActive, const spin_t* spinInact, 
+        const float* dev_bw, const float* rn)
+{
+    int tid = threadIdx.x + blockIdx.x*blockDim.x;
+
+    // coordinates in the 2D (Lx * Ly) lattice 
+    const int x = tid%lx; 
+    const int y = tid/lx;
+
+    const spin_t s0 = spinActive[tid]; // my spin states
+    int nSame=0 ;  // number of neighbors of the same state
+    int site;
+
+    // interaction with horizontal neighbors
+    if(s0==spinInact[tid]) nSame ++;
+
+    if((y%2) == even_odd) {  // neighbor at x' = x-1
+        site = (x==0)? tid+lx-1 : tid-1;
+    } else {                 // neighbor at x' = x+1
+        site = (x==(lx-1))? tid+1-lx : tid+1;
+    }
+    if(s0==spinInact[site]) nSame ++;
+
+    // upper
+    site = (y==(ly-1)) ? tid-lx*(ly-1) : tid+lx;
+    if(s0==spinInact[site]) nSame ++;
+
+    // below
+    site = (y==0)? tid+lx*(ly-1) : tid-lx;
+    if(s0==spinInact[site]) nSame ++;
+
+     // Metropolis algorithm
+     if(rn[tid]<=dev_bw[nSame]) spinActive[tid] = -s0;
+}
+
+// random spin initialization
+__global__ void init_rand_spins(spin_t *spin, const float* rand)
+{
+    int tid = threadIdx.x + blockIdx.x*blockDim.x;
+    if(rand[tid]>0.5) spin[tid] = 1;
+    else spin[tid] = -1;
+}
+
+// local energy and mamgetization in the perspective of A sublattice
+__global__ void mag_energy_local(int lx, int ly, 
+        const spin_t *spinA, const spin_t *spinB, 
+        spin_t *maglocal, spin_t *elocal)
+{
+    int tid = threadIdx.x + blockIdx.x*blockDim.x;
+    int x=tid%lx;
+    int y=tid/lx;
+
+    int en, site;
+
+    // horizontal bonds
+    en = spinB[tid];
+    if(y%2==0) { // neighbor at x-1
+        site = (x==0)? tid+lx-1 : tid-1;
+    } else {     // neighbor at x+1
+        site = (x==lx-1)? tid+1-lx : tid+1;
+    }
+    en += spinB[site];
+
+    // upper
+    site = (y==ly-1)? tid-lx*(ly-1) : tid + lx;
+    en += spinB[site];
+
+    // bottom
+    site = (y==0)? tid+lx*(ly-1) : tid - lx;
+    en += spinB[site];
+
+    elocal[tid]   = -en*spinA[tid];
+    maglocal[tid] = spinA[tid] + spinB[tid];
+}
+
+void get_orderParameter(const int lx, const int ly, 
+       const int nSiteHalf, const spin_t *spinA,
+       const spin_t *spinB, spin_t *localMag, spin_t *localEn,
+       long int *mag, long int *en,
+       const int nBlocks, const int nThreads)
+{
+    mag_energy_local<<<nBlocks, nThreads>>>(lx, ly, spinA, 
+            spinB, localMag, localEn);
+
+    *mag = thrust::reduce(thrust::device_ptr<spin_t>(localMag), 
+            thrust::device_ptr<spin_t>(localMag)+nSiteHalf,0,
+            thrust::plus<long int>());
+    *en  = thrust::reduce(thrust::device_ptr<spin_t>(localEn), 
+            thrust::device_ptr<spin_t>(localEn)+nSiteHalf,0,
+            thrust::plus<long int>());
+}
+
+void set_weight(float *dev_bw, float beta)
+{
+    // bw[n] : spin flip prob. when there are n ferromagnetic neighbors
+    // Ei = -n + (Z-n) = Z-2n
+    // Ef = n - (Z-n) = 2n-Z
+    // bw[n] = exp[-beta(Ef-Ei)] = exp[2*beta*(Z-2n)]
+    
+    float *bw;
+    bw = (float *)malloc((nNeighbor+1)*sizeof(float));
+    
+    for(int n=0; n<=nNeighbor; n++) 
+        bw[n] = exp(2.*beta*(nNeighbor-2*n));
+    cudaMemcpy(dev_bw, bw, sizeof(float)*(nNeighbor+1), cudaMemcpyHostToDevice);
+    free(bw);
+}
+
+// generate two PRNG seeds, one for CPU and another for GPU PRNG
+// they are generated from the std::random_device
+void get_PRNG_seeds(unsigned int* seed0, unsigned int* seed1)
+{
+    std::random_device rd;
+    *seed0 = rd();
+    *seed1 = rd();
+    return;
+}
+
+void prepare1_memory(spin_t **a, spin_t **b, spin_t **c, spin_t **d, int n)
+{
+    cudaMalloc(a, sizeof(spin_t)*n);
+    cudaMalloc(b, sizeof(spin_t)*n);
+    cudaMalloc(c, sizeof(spin_t)*n);
+    cudaMalloc(d, sizeof(spin_t)*n);
+}
+
+void prepare2_memory(float **a, float **b, int n)
+{
+    cudaMalloc(a, sizeof(float)*n);
+    cudaMalloc(b, sizeof(float)*n);
+}
